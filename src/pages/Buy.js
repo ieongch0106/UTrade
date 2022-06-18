d9284453dd0ce2dfc06dec798250aca3fb4adc7a
AryunGupta <aryungupta@gmail.com>
2022-06-18 12:18:24 -0400

    commit

--- src/pages/Buy.js
+++ src/pages/Buy.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Button, Form } from '../components/Components'
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
 
 export default function Buy() {
-  
+
   return (
-    <div>Buy</div>
+    <Form width='95vw' textAlign= 'left' paddingLeft="20px" />
   )
 }

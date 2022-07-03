86f2d3f8d3af09f71729f36821332f0f50fe8649
AryunGupta <aryungupta@gmail.com>
2022-07-03 12:52:31 -0400

    co

--- mongo_folder/server.js
+++ mongo_folder/server.js
@@ -1,15 +1,14 @@
 // import express from "express"
 // import cors from "cors"
 
-// mongo username- utrade
-// mongo password phMaKOfw0ZvHo1ag
+
 
 // const app = express()
 // app.use(cors())
 // app.use(express.json())
 
 const {MongoClient} = ('mongodb').MongoClient
-const uri = 'mongodb+srv://utrade:phMaKOfw0ZvHo1ag@cluster0.v7b4d.mongodb.net/?retryWrites=true&w=majority'
+uri = ""
 const client = new MongoClient(uri)
 client.connect();
 
